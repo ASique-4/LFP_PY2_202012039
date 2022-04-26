58287985be7760419603400c6fc1d6728f6f02a3
ASique-4 <3555772080101@ingenieria.usac.edu.gt>
2022-04-25 21:21:30 -0600

    Ventana

---
+++ main.py
@@ -0,0 +1,24 @@
+
+import PySimpleGUI as sg
+
+
+sg.theme('GreenTan')
+
+layout = [[sg.Text('Your output will go here', size=(40, 1))],
+          [sg.Output(size=(110, 20), font=('Helvetica 10'))],
+          [sg.Multiline(size=(70, 5), enter_submits=False, key='-QUERY-', do_not_clear=False),
+           sg.Button('SEND', button_color=(sg.YELLOWS[0], sg.BLUES[0]), bind_return_key=True),
+           sg.Button('EXIT', button_color=(sg.YELLOWS[0], sg.GREENS[0]))]]
+
+window = sg.Window('Chat window', layout, font=('Helvetica', ' 13'), default_button_element_size=(8,2), use_default_focus=False)
+
+while True:     
+    event, value = window.read()
+    if event in (sg.WIN_CLOSED, 'EXIT'): 
+        break
+    if event == 'SEND':
+        query = value['-QUERY-'].rstrip()
+
+        print('The command you entered was {}'.format(query), flush=True)
+
+window.close()
